b2f246ae2e8dbcf4601d399eaee258f13d2205fe
Md Minhajul Karim <121756847+minhajahad@users.noreply.github.com>
2023-03-05 00:10:31 +0600

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,20 +22,43 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/login', function(){
+//     session()->put('user_id',5);
+//     echo "You are now logged in";
+// });
+
+// Route::get('/logout', function(){
+//     session()->forget('user_id',5);
+//     echo "Logged out";
+// });
+
+Route::get('/no_access', function(){
+    echo "You are not allowed to view this page";;
+});
+
+
 
 Route::get('/user',[UserController::class, 'getUser']);
 Route::get('/register',[RegisterController::class, 'getUser']);
 Route::post('/register',[RegisterController::class, 'registerUser']);
-Route::get('/customer',[CustomerController::class, 'index']);
-Route::post('/customer',[CustomerController::class, 'storeData']);
-Route::get('/customer/viewcustomer',[CustomerController::class, 'getcustomerview']);
+Route::get('/customer',[CustomerController::class, 'index'])->middleware('ageUserStudent');
+Route::post('/customer',[CustomerController::class, 'storeData'])->middleware('ageUserStudent');
+Route::get('/customer/viewcustomer',[CustomerController::class, 'getcustomerview'])->name('customer.view')->middleware('ageUserStudent');
 Route::get('/customer/delete/{id}',[CustomerController::class, 'deleteCustomer']);
 Route::get('/customer/edit/{id}',[CustomerController::class, 'editCustomer']);
 Route::post('/customer/update/{id}',[CustomerController::class, 'update']);
 
-Route::get('/student',[StudentController::class,'index']);
-Route::get('/addstudent',[StudentController::class,'create']);
-Route::post('/addstudent',[StudentController::class,'store']);
-Route::get('/student/delete/{id}',[StudentController::class, 'deleteStudent']);
-Route::get('/student/edit/{id}',[StudentController::class, 'editStudent']);
-Route::post('/student/update/{id}',[StudentController::class, 'update']);
+
+Route::middleware(['ageUserStudent'])->group(function(){
+    Route::get('/student',[StudentController::class,'index']);
+    Route::get('/addstudent',[StudentController::class,'create']);
+    Route::post('/addstudent',[StudentController::class,'store']);
+    Route::get('/student/delete/{id}',[StudentController::class, 'deleteStudent']);
+    Route::get('/student/edit/{id}',[StudentController::class, 'editStudent']);
+    Route::post('/student/update/{id}',[StudentController::class, 'update']);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/admin',[App\Http\Controllers\AdminController::class, 'index']);
---
+++ storage/framework/views/c32c292304680d1b02e04726610b3f5f1071a795.php
@@ -0,0 +1,50 @@
+
+<?php $__env->startSection('title','Add New Student'); ?>
+<?php $__env->startSection('page-content'); ?>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <?php if(session('status')): ?>
+                    <h6 class="alert alert-success"><?php echo e(session('status')); ?></h6>
+                    <?php endif; ?>
+                    <div class="card-header">
+                        <h4>Add Students Info
+                            <a href="<?php echo e(url('/')); ?>/student" class="btn btn-danger float-right">Back</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="<?php echo e(url('/')); ?>/add_student" method="post" enctype="multipart/form-data">
+                            <?php echo csrf_field(); ?> 
+
+                            <div class="form-group mb-3">
+                                <label for="">Student Name</label>
+                                <input type="text" name="name" id="name" class="form-control">
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label for="">Gender</label><br>
+                                <input type="radio" name="gender" id="gender" value="male">Male
+                                <input type="radio" name="gender" id="gender" value="female">Female
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label for="">Course</label>
+                                <input type="text" name="course" id="course" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Profile Photo</label>
+                                <input type="file" name="image" id="image" class="form-control">
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <button type="submit" class="btn btn-primary">Save Student</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('students.studentmaster', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Xampp\htdocs\demo_project\resources\views/students/create.blade.php ENDPATH**/ ?>
